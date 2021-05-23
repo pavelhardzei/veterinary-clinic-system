ea1f749437d1b5f575bca6259c601bdfd141fbaf
pavelhardzei <pavelgordei11@gmail.com>
2021-05-23 15:09:58 +0300

    added crud form skeleton

---
+++ crud_form.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+from tkinter import ttk
+import psycopg2
+
+
+class CRUDForm:
+
+    def __init__(self, root: tk.Tk, connection, cursor):
+        self.__connection = connection
+        self.__cursor = cursor
+
+        self.__crud_form = tk.Toplevel(root)
+        self.__crud_form.title("CRUD Form")
+        self.__width = 600
+        self.__height = 400
+        self.__crud_form.wm_minsize(self.__width, self.__height)
+        self.__crud_form.geometry("+{}+{}".
+                                  format(int(self.__crud_form.winfo_screenwidth() / 2 - self.__width / 2),
+                                         int(self.__crud_form.winfo_screenheight() / 2 - self.__height / 2)))
+        self.__crud_form.protocol("WM_DELETE_WINDOW", lambda: root.destroy())
+
+        self.__root_frame = tk.Frame(master=self.__crud_form)
+
+        self.__headline_frame = tk.Frame(master=self.__root_frame)
+
+        __lbl = tk.Label(master=self.__headline_frame, text="Tables", font="Times 16")
+        __lbl.grid(row=0, column=0, columnspan=4, pady=15)
+
+        __appointments = tk.Button(master=self.__headline_frame, text="Appointments", background="#555",
+                                   foreground="#ccc", padx="10", pady="5", font="Times 14",
+                                   command=self.__open_appointments)
+        __appointments.grid(row=1, column=0, padx=10)
+        __clients = tk.Button(master=self.__headline_frame, text="Clients", background="#555", foreground="#ccc",
+                              padx="10", pady="5", font="Times 14",
+                              command=self.__open_clients)
+        __clients.grid(row=1, column=1, padx=10)
+        __patients = tk.Button(master=self.__headline_frame, text="Patients", background="#555", foreground="#ccc",
+                               padx="10", pady="5", font="Times 14",
+                               command=self.__open_patients)
+        __patients.grid(row=1, column=2, padx=10)
+        __doctors = tk.Button(master=self.__headline_frame, text="Doctors", background="#555", foreground="#ccc",
+                              padx="10", pady="5", font="Times 14",
+                              command=self.__open_doctors)
+        __doctors.grid(row=1, column=3, padx=10)
+
+        self.__headline_frame.grid(row=0, column=0)
+
+        self.__table_frame = tk.Frame(master=self.__root_frame)
+
+        self.__table = ttk.Treeview(master=self.__table_frame)
+        self.__table.column("#0", width=int(self.__width * 0.9))
+        self.__table.grid(row=0, column=0)
+
+        self.__scrollbar = ttk.Scrollbar(master=self.__table_frame, orient=tk.VERTICAL, command=self.__table.yview)
+        self.__table.configure(yscroll=self.__scrollbar.set)
+        self.__scrollbar.grid(row=0, column=1, sticky="ns")
+
+        self.__table_frame.grid(row=1, column=0, pady=30)
+
+        self.__root_frame.pack()
+
+    def __open_appointments(self):
+        self.__create_table(("id", "client_id", "pet_id", "date", "time"))
+
+    def __open_clients(self):
+        self.__create_table(("id", "name", "telephone", "pet_id"))
+
+    def __open_patients(self):
+        self.__create_table(("id", "name", "age", "owner_id", "kind"))
+
+    def __open_doctors(self):
+        self.__create_table(("id", "name", "salary"))
+
+    def __create_table(self, columns):
+        self.__table.destroy()
+        self.__table = ttk.Treeview(master=self.__table_frame)
+        self.__table['columns'] = columns
+        self.__table.column("#0", width=0, stretch=tk.NO)
+        self.__table.heading("#0", text='')
+        for column in columns:
+            self.__table.column(column, anchor=tk.CENTER, width=int(0.9 * self.__width / len(columns)))
+            self.__table.heading(column, text=column, anchor=tk.CENTER)
+        self.__table.grid(row=0, column=0)
+
+    def __fill_table(self):
+        pass
